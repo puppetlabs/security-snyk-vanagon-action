bf5e46335fc9974c12bb4b89b8e359ba46ead3f9
Jeremy Mill <jeremymill@gmail.com>
2021-11-18 13:09:14 -0500

    add git ssh command

--- docker_confs/start_script.sh
+++ docker_confs/start_script.sh
@@ -8,12 +8,7 @@ echo "# github.com:22 SSH-2.0-babeld-a73e1397" >> ~/.ssh/known_hosts
 echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
 echo "# github.com:22 SSH-2.0-babeld-a73e1397" >> ~/.ssh/known_hosts
 echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" >> ~/.ssh/known_hosts
-# write a config
-echo "Host *" >> ~/.ssh/config
-echo "    StrictHostKeyChecking yes" >> ~/.ssh/config
-echo "    UserKnownHostsFile ~/.ssh/known_hosts" >> ~/.ssh/config
 chmod 600 ~/.ssh/known_hosts
-chmod 600 ~/.ssh/config
 # handle user SSH keys
 if [ "$INPUT_SSHKEY" ];
 then
@@ -22,11 +17,16 @@ then
         filename="/.ssh/$INPUT_SSHKEYNAME"
         echo $INPUT_SSHKEY | base64 -d > ~/"$filename"
         chmod 600 ~/"$filename"
+        export FullKnownHosts=`realpath -z ~/.ssh/known_hosts`
+        export FullKeyPath=`realpath -z ~/.ssh/$INPUT_SSHKEYNAME`
+        # git ssh command
+        export GIT_SSH_COMMAND='ssh -i $FullKeyPath -o UserKnownHostsFile=$FullKnownHosts' 
     else
         echo "ERROR: SSHKEY set with no SSHKEYNAME"
         exit 1
     fi
 fi
+
 # test
 cat ~/.ssh/known_hosts
 git ls-remote git@github.com:puppetlabs/enterprise_tasks.git
